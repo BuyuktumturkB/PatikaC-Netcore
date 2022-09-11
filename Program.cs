9bb901d804fb934998e31f30fcd84089b3a4cbaa
BuyuktumturkB <102427841+BuyuktumturkB@users.noreply.github.com>
2022-09-11 15:51:56 +0300

    Add files via upload
    Farklı değişken tipleri ile kod örnekleri

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HarckerRank1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a;
+			int b;
+			 a = Convert.ToInt32(Console.ReadLine());
+			 b = Convert.ToInt32(Console.ReadLine());
+			int sum = a + b;
+			int difference = a - b;
+			Console.WriteLine(sum);
+			Console.WriteLine(difference);
+			string password;
+			bool passwordcontrol;
+			Console.WriteLine("Please Enter Password: ");
+			password = Console.ReadLine();
+			
+			if (password == "WE LOVE KEBAB") 
+			{
+				passwordcontrol = true;
+				Console.WriteLine("Password Correct");
+			}
+			else
+			{
+				passwordcontrol = false;
+				Console.WriteLine("Wrong Password");
+			}
+
+			double c;
+			c = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine($"{c - 2.27}");
+			
+
+		}
+	}
+}
