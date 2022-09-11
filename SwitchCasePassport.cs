dc895195f7215899e0e84efa25aec6c3ef5cd603
BuyuktumturkB <102427841+BuyuktumturkB@users.noreply.github.com>
2022-09-11 16:47:59 +0300

    Add files via upload

---
+++ SwitchCasePassport.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HarckerRank1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		
+			Console.WriteLine("Please Enter Your Nationality: ");
+			string Nationality;
+			Nationality = Console.ReadLine();
+			switch (Nationality)
+			{
+				case "EU Nation":
+					Console.WriteLine("No Need Visa");
+					break;
+				case "Japan Citizien":
+					Console.WriteLine("No Need Visa");
+					break;
+				case "Balkan Countries":
+					Console.WriteLine(" Visa Required");
+					break;
+				case "African Countries":
+					Console.WriteLine("Visa Required");
+					break;
+
+
+			}
+
+			}
+
+		}
+	}
+}
