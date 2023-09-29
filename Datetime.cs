96c233a98d0fe4743da9c4155448f0ba1aa431d1
Cahit Burak Buyktumturk <102427841+BuyuktumturkB@users.noreply.github.com>
2023-09-29 16:28:54 +0300

    Create Datetime.cs

---
+++ Datetime.cs
@@ -0,0 +1,13 @@
+Console.WriteLine(DateAndTime.Now.Day);
+Console.WriteLine(DateAndTime.Now.Date);
+Console.WriteLine(DateAndTime.Now.DayOfWeek);
+
+Console.WriteLine(DateAndTime.Now.Hour);
+Console.WriteLine(DateAndTime.Now.DayOfYear);
+Console.WriteLine(DateAndTime.Now.ToLongTimeString());
+Console.WriteLine(DateAndTime.Now.ToShortTimeString());
+
+Console.WriteLine(DateAndTime.Now.AddDays(3));
+Console.WriteLine(DateAndTime.Now.AddHours(4));
+
+Console.WriteLine(DateAndTime.Now.ToString("yyyy"));
